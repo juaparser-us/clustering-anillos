8d38e01d1c9a914dcb3e69b712b44c101573d6b7
juaparser <46746374+juaparser@users.noreply.github.com>
2020-06-11 13:28:07 +0200

    Objetivo 1 completado; Archivo tp_extended.py

--- tp_extended.py
+++ tp_extended.py
@@ -1,35 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-print("Input three coordinate of the circle:")
-puntos1 = [(9, 7), (7.7, 6.5), (7, 5), (11, 5), (9, 3), (10.3, 3.5), 
-            (7.3, 4), (10.3, 6.5), (3, 2), (2, 3), (1, 2), (2, 1), (1.2, 1.4), 
-            (1.2, 2.6), (2.7, 1.3), (2.8, 2.6)]
+points = [(9, 7), (7.7, 6.5), (7, 5), (11, 5), (9, 3), (10.3, 3.5), 
+            (7.3, 4), (10.3, 6.5)]
 
-x = []
-y = []
-for a in puntos1:
-    x.append(a[0]);
-    y.append(a[1]);
-    
-c = (x1-x2)**2 + (y1-y2)**2;
-a = (x2-x3)**2 + (y2-y3)**2;
-b = (x3-x1)**2 + (y3-y1)**2;
-s = 2*(a*b + b*c + c*a) - (a*a + b*b + c*c) ;
-px = (a*(b+c-a)*x1 + b*(c+a-b)*x2 + c*(a+b-c)*x3) / s;
-py = (a*(b+c-a)*y1 + b*(c+a-b)*y2 + c*(a+b-c)*y3) / s ;
-ar = a**0.5;
-br = b**0.5;
-cr = c**0.5 ;
-r = ar*br*cr / ((ar+br+cr)*(-ar+br+cr)*(ar-br+cr)*(ar+br-cr))**0.5;
-print("Radius of the said circle:");
-print("{:>.3f}".format(r));
-print("Central coordinate (x, y) of the circle:");
-print("{:>.3f}".format(px),"{:>.3f}".format(py));
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+centroid = (sum(x) / len(points), sum(y) / len(points))
+x.append(centroid[0])
+y.append(centroid[1])
 
-x = [x1,x2,x3,px];
-y = [y1,y2,y3,py];
+xc = centroid[0];
+yc = centroid[1];
 
-plt.scatter(x, y);
+m = [math.sqrt(math.pow(xc-p[0],2) + math.pow(yc-p[1],2)) for p in points];
+
+r = np.mean(m);
+
+fig, ax = plt.subplots()
+
+ax.set(xlim=(5, 15), ylim = (0, 10))
+
+a_circle = plt.Circle((centroid[0], centroid[1]), r,fill=False)
+ax.add_artist(a_circle);
+ax.scatter(x,y);
 plt.show();
 
+print(centroid)
