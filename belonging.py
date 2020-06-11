1e48c6ee61b8505f5d2868d15c01f5b9e7052d6f
antmaczam <antoniomaciaszambrano24@gmail.com>
2020-06-11 15:27:05 +0200

    [New] Add get membership method

---
+++ belonging.py
@@ -0,0 +1,30 @@
+import math
+
+def get_belongings(p,clusters):
+    belongings = []
+    norm = 0
+    for x in clusters:
+        d = abs(distance(p,x[1])-x[2])
+        belongings.append((x[0],d))
+        norm += d
+    
+    result = []
+    for x in belongings:
+        result.append((x[0],x[1]/norm))
+
+    return result
+
+def distance(org,dest):
+    return math.sqrt(pow(dest[0]-org[0],2)+pow(dest[1]-org[1],2))
+
+""" Test the method to obtain the degree of membership for a point
+cl1 = ["a",(1,1),3]
+cl2 = ["b",(5,4),2]
+cl3 = ["c",(7,9),3.5]
+
+clusters = [cl1, cl2, cl3]
+
+p = (17,10)
+
+print(get_belongings(p,clusters))
+"""
